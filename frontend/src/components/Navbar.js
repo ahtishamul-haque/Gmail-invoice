12d352c4d9f83f2ca56118c0caca9eb5e3cb0be1
Ahtishamul Haque <112795299+ahtishamul-haque@users.noreply.github.com>
2025-09-06 20:12:39 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,10 +4,11 @@ import { authUrl } from "../services/api";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="brand">📧 Receet</div>
+      <div className="brand">📧 E-Receipt Aggregation</div>
       <div className="actions">
         <a className="btn" href={authUrl()}>Connect Gmail</a>
       </div>
     </nav>
   );
 }
+
